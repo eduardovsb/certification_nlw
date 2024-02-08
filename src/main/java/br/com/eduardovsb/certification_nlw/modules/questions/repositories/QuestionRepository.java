1610b04b7527c4ca615608bed696125e93de3169
Gabriel <gaah4gms@gmail.com>
2024-02-08 18:14:07 -0300

    first commit

---
+++ src/main/java/br/com/eduardovsb/certification_nlw/modules/questions/repositories/QuestionRepository.java
@@ -0,0 +1,14 @@
+package br.com.eduardovsb.certification_nlw.modules.questions.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.eduardovsb.certification_nlw.modules.questions.entities.QuestionEntity;
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<QuestionEntity, UUID> {
+
+  List<QuestionEntity> findByTechnology(String technology);
+
+}
---
+++ src/main/java/br/com/eduardovsb/certification_nlw/modules/students/controllers/StudentController.java
@@ -0,0 +1,48 @@
+package br.com.eduardovsb.certification_nlw.modules.students.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.eduardovsb.certification_nlw.modules.students.dto.StudentCertificationAnswerDTO;
+import br.com.eduardovsb.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import br.com.eduardovsb.certification_nlw.modules.students.entities.CertificationStudentEntity;
+import br.com.eduardovsb.certification_nlw.modules.students.useCases.StudentCertificationAnswersUseCase;
+import br.com.eduardovsb.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+  @Autowired
+  private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
+  @Autowired
+  private StudentCertificationAnswersUseCase studentCertificationAnswersUseCase;
+
+  @PostMapping("/verifyIfHasCertification")
+  public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyIfHasCertificationDTO) {
+    var result = this.verifyIfHasCertificationUseCase.execute(verifyIfHasCertificationDTO);
+    if (result) {
+      return "Usuário já fez a prova";
+    }
+
+    return "Usuário pode fazer a prova";
+  }
+
+  @PostMapping("/certification/answer")
+  public ResponseEntity<Object> certificationAnswer(
+      @RequestBody StudentCertificationAnswerDTO studentCertificationAnswerDTO) {
+    try {
+      var result = this.studentCertificationAnswersUseCase.execute(studentCertificationAnswerDTO);
+
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+}
